ec66471f2edcb7653273348b1ff5d6f9d2108778
Tiago Luchini <tiago@luchini.nyc>
2020-01-02 22:35:19 -0500

    Most functionality for commands done and tested

--- dev/user.clj
+++ dev/user.clj
@@ -5,87 +5,6 @@
             [clojure.string :as s]
             [clojure.core.async :refer [<! >! <!! >!! go go-loop chan pipeline]]))
 
-#_(defn read-is [^java.io.InputStream is]
-    (let [bufsize 8192
-          buf (byte-array bufsize)]
-      (loop [cout ""]
-        (let [n (.read is buf)
-              this-bit (->> buf
-                            (take n)
-                            byte-array
-                            String.)]
-          #_(println (take-last 2 this-bit))
-          (println "\n= 13 10"
-                   (= 13 (byte (first (take-last 2 this-bit))))
-                   (= 10 (byte (last (take-last 2 this-bit)))))
-          (println "first" (byte (first (take-last 2 this-bit))))
-          (println "last" (byte (last (take-last 2 this-bit))))
-          
-          (if (or (pos? (.available is))
-                  (not= '(13 10) (map byte (take-last 2 this-bit))))
-            (recur (str cout this-bit))
-            (str cout (apply str (drop-last 2 this-bit))))))))
-
-#_(defn read-is2 [^java.io.InputStream is]
-    (let [bufsize 8192
-          buf (byte-array bufsize)]
-      (loop [cout ""]
-        (let [n (.read is buf)
-              out (str cout
-                       (->> buf
-                            (take n)
-                            byte-array
-                            String.))]
-          (if (pos? (.available is))
-            (recur out)
-            out)))))
-
-
-#_(defn cmd-splitter-xform
-    [rf]
-    (println "\n= xform")
-    (let [cmd (volatile! "")
-          crlf (volatile! 0)]
-      (fn
-        ([] (println "- 0 args") (rf))
-        ([result]
-         (println "- 1 args")
-         (if-not (= @cmd "")
-           result
-           (let [cmd-out @cmd]
-             (vreset! cmd "")
-             (vreset! crlf 0)
-             (unreduced (rf result cmd-out)))))
-        ([result input]
-         (println "- 2 args (result:" result "input:" input ", cmd:" @cmd ")")
-         (cond
-           (and (= 1 @crlf) (= input 10))
-           (vreset! crlf 2)
-           
-           (= input 13)
-           (vreset! crlf 1)
-
-           :otherwise
-           (vswap! cmd #(str % (char input))))
-         
-         (if (= @crlf 2)
-           (let [cmd-out @cmd]
-             (vreset! cmd "")
-             (vreset! crlf 0)
-             (let [step (rf result cmd-out)]
-               step))
-           #_result
-           nil)))))
-
-
-;; should yield ["io.send(\"system.lua\")" "foo" and leave bar [98 97 114] pending
-#_(eduction cmd-splitter-xform [105 111 46 115 101
-                                110 100 40 34 115 121
-                                115 116 101 109 46 108
-                                117 97 34 41 13 10 102
-                                111 111 13 10 98 97 114])
-
-
 (def ^:private chunksize 255)
 
 (def ^:private state (atom 'pending))
@@ -99,13 +18,13 @@
   (reset! entry "")
   (reset! crlf :none))
 
-
 (defn ^:private transition-to!
   ([to]
    (transition-to! to nil))
   ([to opts]
    (let [from @state]
      (if (or (and (or (= from 'pending)
+                      (= from 'ready)
                       (= from 'cmd)
                       (= from 'receive-mode))
                   (= to 'ready))
@@ -124,89 +43,116 @@
        (reset! state (vary-meta to assoc :opts opts))
        (throw (ex-info "Illegal transition" {:from from :to to}))))))
 
-(defn ^:private wait-for [target-state]
-  (loop []
-    (if-not (= @state target-state)
-      (recur))))
+(declare ^:private force-ready!)
+
+(defn ^:private wait-for [board target-state]
+  (let [start (. System (nanoTime))
+        retry-count (atom 0)]
+    (loop []
+      (Thread/sleep 5)
+      (when (>= (- (. System (nanoTime)) start)
+                500000000.0)
+        (if (< @retry-count 5)
+          (do (swap! retry-count inc)
+              (force-ready! board))
+          (throw (ex-info "Timed out" {:waiting-for target-state}))))
+      (if-not (= @state target-state)
+        (recur)))))
 
 (defn ^:private send-command* [{:keys [port] :as board} cmd]
-  (let [raw-cmd (.getBytes (str cmd "\r\n"))]
-    (serial/write port raw-cmd)))
+(let [raw-cmd (.getBytes (str cmd "\r\n"))]
+  (serial/write port raw-cmd)))
 
 (defn send-command [{:keys [port] :as board} cmd]
-  (wait-for 'ready)
+  (wait-for board 'ready)
   (transition-to! 'cmd cmd)
   (send-command* board cmd))
 
+(defn ^:private force-ready! [board]
+  (send-command* board "")
+  (wait-for board 'ready))
+
 (defn ^:private start-receive-mode! [{:keys [port] :as board} file-name]
-  (wait-for 'ready)
+  (wait-for board 'ready)
   (transition-to! 'receive-mode)
   (let [cmd (str "io.receive(\"" file-name "\")\r")]
     (serial/write port (.getBytes cmd))))
 
 (defn ^:private write-chunk! [{:keys [port] :as board} n buf]
-  (wait-for 'ready-to-receive)
+  (wait-for board 'ready-to-receive)
   (serial/write port (byte-array [n]))
   (serial/write port (->> buf
                           (take n)
                           byte-array)))
 
 (defn ^:private finalize-file! [{:keys [port] :as board}]
-  (wait-for 'ready-to-receive)
+  (wait-for board 'ready-to-receive)
   (serial/write port (byte-array [0])))
 
 (defn write-file!
-  ([board file-name]
-   (write-file! board file-name file-name))
-  ([board src-file-name dst-file-name]
-   (let [fis (io/input-stream (io/file src-file-name))]
-     (start-receive-mode! board dst-file-name)
-     (loop []
-       (let [buf (byte-array chunksize)
-             n (.read fis buf)]
-         (if (pos? n)
-           (do (println "Read and will write" n "bytes")
-               (write-chunk! board n buf)
-               (recur))
-           (finalize-file! board)))))))
+([board file-name]
+ (write-file! board file-name file-name))
+([board src-file-name dst-file-name]
+ (let [fis (io/input-stream (io/file src-file-name))]
+   (start-receive-mode! board dst-file-name)
+   (loop []
+     (let [buf (byte-array chunksize)
+           n (.read fis buf)]
+       (if (pos? n)
+         (do (println "Read and will write" n "bytes")
+             (write-chunk! board n buf)
+             (recur))
+         (finalize-file! board)))))))
 
 (defn ^:private parse-entry [e]
   #_(println (str "Parsing: '" e "'"))
+  #_(println "- Parser will"
+             (cond
+               (= "/ > " e)
+               "transition to ready"
+
+               (and (= 'cmd @state)
+                    (not= (-> @state meta :opts first) e))
+               "console out"
+               
+               :otherwise
+               "not recognize"))
   (cond
     (= "/ > " e)
     (transition-to! 'ready)
 
-    (and (= 'cmd @state)
-         (not= (-> @state meta :opts first) e))
-    (do
-      #_(println "Console out:" e))
+    (= 'cmd @state)
+    (when (not= (-> @state meta :opts) e)
+      (println e))
+
+    (= "")
+    (do)
     
     :otherwise
     (do
-      #_(println "UNKNOWN!" e))))
+      (println "UNKNOWN!" (-> e .getBytes vec)))))
 
 (defn ^:private rx-listener [rx-chan]
-  (fn [is]
-    (let [buf (byte-array chunksize)
-          n (.read is buf)
-          parsed (->> buf
-                      (take n)
-                      byte-array
-                      String.)]
-      #_(println "Read" n "bytes")
-      (go (doseq [b (->> buf
+(fn [is]
+  (let [buf (byte-array chunksize)
+        n (.read is buf)
+        parsed (->> buf
+                    (take n)
+                    byte-array
+                    String.)]
+    (go (doseq [b (->> buf
+                       (take n)
+                       byte-array)]
+          (>! rx-chan b)))
+    #_(println "\n====\nRead" n "bytes")
+    #_(println " =" (->> buf
                          (take n)
-                         byte-array)]
-            (>! rx-chan b)))
-      #_(println "\n====\nRead" n "bytes")
-      #_(println " =" (->> buf
-                           (take n)
-                           byte-array
-                           vec))
-      #_(println " =" (->> buf
-                           (take n)
-                           byte-array
-                           String.)))))
+                         byte-array
+                         vec))
+    #_(println " =" (->> buf
+                         (take n)
+                         byte-array
+                         String.)))))
 
 (defn connect-board! [port-id]
   (reset-state!)
@@ -224,7 +170,7 @@
 
     (go-loop []
       (let [x (<! rx-chan)]
-        (print "Seeing" x "with crlf as" @crlf)
+        #_(print "Seeing" x "with crlf as" @crlf)
         (cond
           ;; cr has already seen and now lf, so crlf is true
           (and (= :cr @crlf) (= x 10))
@@ -252,9 +198,10 @@
           
           :otherwise
           (swap! entry #(str % (char x))))
-        (println " crlf now is" @crlf)
+        #_(println " crlf now is" @crlf)
         (when (or (= @crlf :crlf) (= @crlf :control-ack))
-          (do (case @crlf
+          (do #_(println "Putting entry in queue")
+              (case @crlf
                 :crlf
                 (let [entry-out @entry]
                   (>! entries-chan entry-out))
@@ -267,22 +214,39 @@
     (let [board {:port port
                  :rx-chan rx-chan
                  :entries-chan entries-chan}]
-      (send-command* board "")
+      (force-ready! board)
       board)))
 
 (defn disconnect-board! [{:keys [port] :as board}]
   (serial/close! port)
   true)
 
-(def board (connect-board! "/dev/cu.SLAB_USBtoUART"))
+#_(def board (connect-board! "/dev/cu.SLAB_USBtoUART"))
 
-(send-command board "os.version()")
+#_(send-command board "os.version()")
 
 #_(send-command board "os.ls()")
+
+#_(send-command board "os.remove(\"touch.lua\")")
 
 #_(send-command board "os")
 
 #_(disconnect-board! board)
+
+#_(dotimes [n 500]
+    (let [b (connect-board! "/dev/cu.SLAB_USBtoUART")]
+      #_(Thread/sleep 10)
+      (when-not (= @state 'ready)
+        (println (str "State is " @state " instead of ready"))
+        (Thread/sleep 500)
+        (println (str "!!! State is " @state " after waiting 500ms"))
+        (Thread/sleep 500)
+        (println (str "!!! State is " @state " after waiting 500ms"))
+        (Thread/sleep 500)
+        (println (str "!!! State is " @state " after waiting 500ms"))
+        (Thread/sleep 500)
+        (println (str "!!! State is " @state " after waiting 500ms")))
+      (disconnect-board! b)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
